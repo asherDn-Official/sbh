15e50c8f3e8da2732b2f96d70a6456dddc2c55d0
AkilanRittz <akilan@rittzdigital.com>
2026-04-02 18:05:57 +0530

    open-graph for each properties

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -2,13 +2,17 @@ import PropertyPageClient from "./propertiesPage";
 
 const seoData = {
   katrambakkam: {
-    title: "Katrambakkam Properties | Sri Balaji Homes",
-    description: "Explore premium properties in Katrambakkam.",
+    title: "Resale Plots in Kattrambakkam | Near Sriperumbudur | Sri Balaji Homes",
+    description: "Looking for resale plots in Kattrambakkam? Sri Balaji Homes offers CMDA & DTCP approved land near Sriperumbudur SIPCOT. Clear titles and immediate registration. Zero brokerage!",
   },
   nandhambakkam: {
-    title: "Nandhambakkam Properties | Sri Balaji Homes",
-    description: "Explore premium properties in Nandhambakkam.",
+    title: "Resale Plots in Nandhambakkam | Near Kundrathur | Sri Balaji Homes",
+    description: "Buy premium CMDA approved resale plots in Nandhambakkam near Kundrathur. Sri Balaji Homes offers legally verified residential land ready for immediate construction.",
   },
+  thaiyur: {
+    title: "Resale Plots in Thaiyur OMR | Near Siruseri IT Park | Sri Balaji Homes",
+    description: "Invest in DTCP approved resale plots in Thaiyur, OMR. Sri Balaji Homes offers premium land near Siruseri IT Park and SSN College. Immediate registration available!"
+  }
 };
 
 export async function generateMetadata({ params }) {
@@ -24,6 +28,22 @@ export async function generateMetadata({ params }) {
 
     alternates: {
       canonical: `https://sribalajihomes.in/${slug}`,
+    },
+     openGraph: {
+      title: data?.title || `${place} Properties | Sri Balaji Homes`,
+      description: data?.description || "Buy CMDA & DTCP approved resale plots in Chennai directly from Sri Balaji Homes. Premium land in Kattrambakkam, Nandhambakkam & Thaiyur. Zero brokerage!",
+      url: `https://sribalajihomes.in/${slug}`,
+      siteName: "Sri Balaji Homes",
+      images: [
+        {
+          url: "/android-chrome-512x512.png",
+          width: 486,
+          height: 486,
+          alt: "Sri Balaji Homes",
+        },
+      ],
+      locale: "en_IN",
+      type: "website",
     },
   };
 }
